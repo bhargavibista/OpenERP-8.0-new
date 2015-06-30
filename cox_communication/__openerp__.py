7c0c260b40898707ab0beec8def3017695c26dc0
yogita <yogita.paghdar@bistasolutions.com>
2015-06-30 11:49:25 +0530

    no changes

--- cox_communication/__openerp__.py
+++ cox_communication/__openerp__.py
@@ -113,7 +113,7 @@ This module is built to handle requirements of Cox Communication.
 	
 	'sale_import_view.xml',
         'mrp_view.xml',
-        'service_config_view.xml',
+#        'service_config_view.xml',
     ],
     'demo_xml': [],
     'js':['static/src/js/*.js'],
