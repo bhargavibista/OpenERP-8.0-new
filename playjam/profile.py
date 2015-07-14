908bb916ac6ed3314b4929c29cfa3df1b6912c8f
saziya <saziya@saziya-desktop>
2015-07-14 12:27:01 +0530

    changes done to hide credit and cancellation menu item

--- playjam/profile.py
+++ playjam/profile.py
@@ -18,3 +18,7 @@ class user_profile(models.Model):
     pc_params = fields.Char()
     playjam_exported = fields.Boolean()
     player_tag = fields.Char(string='Player Tag',size=100)
+    _defaults={
+    'gender':'O'
+    }
+    _sql_constraints = [('playertag_uniq', 'unique(player_tag)', 'The Player Tag already exists.')]
