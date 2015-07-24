9b674791535daa0d1c8d3bef3e0b8ce9837603e5
saziya <saziya@saziya-desktop>
2015-07-24 15:14:49 +0530

    migrated new changes

--- cox_communication/wizard/warning_message.py
+++ cox_communication/wizard/warning_message.py
@@ -1,5 +1,5 @@
-from osv import osv
-from osv import fields
+from openerp.osv import osv
+from openerp.osv import fields
 from openerp.tools.translate import _
 
 WARNING_TYPES = [('warning',' '),('info','Information'),('error','Error')]
@@ -18,10 +18,10 @@ class warning(osv.osv_memory):
         """Get the view id
         @return: view id, or False if no view found
         """
-        res = self.pool.get('ir.model.data').get_object_reference(cr, uid,'elemental_wellness', 'warning_form')
+        res = self.pool.get('ir.model.data').get_object_reference(cr, uid,'cox_communication', 'warning_form')
         return res and res[1] or False
 
-    def message(self, cr, uid, id, context):
+    def message_info(self, cr, uid, id, context):
         message = self.browse(cr, uid, id)
         message_type = [t[1]for t in WARNING_TYPES if message.type == t[0]][0]
         res = {
@@ -37,17 +37,17 @@ class warning(osv.osv_memory):
         }
         return res
 
-    def warning(self, cr, uid, title, message, context=None):
+    def warning_message(self, cr, uid, title, message, context=None):
         id = self.create(cr, uid, {'title': title, 'message': message, 'type': 'warning'})
-        res = self.message(cr, uid, id, context)
+        res = self.message_info(cr, uid, id, context)
         return res
 
     def info(self, cr, uid, title, message, context=None):
         id = self.create(cr, uid, {'title': title, 'message': message, 'type': 'info'})
-        res = self.message(cr, uid, id, context)
+        res = self.message_info(cr, uid, id, context)
         return res
 
     def error(self, cr, uid, title, message, context=None):
         id = self.create(cr, uid, {'title': title, 'message': message, 'type': 'error'})
-        res = self.message(cr, uid, id, context)
+        res = self.message_info(cr, uid, id, context)
         return res
